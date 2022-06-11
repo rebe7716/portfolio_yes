a1dfcb47614ac3917b68610b780e0255ae8f9dec
rebe7716 <rebe7716@stud.kea.dk>
2022-06-11 12:44:44 +0200

    ændringer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      /*sort*/
+      black: "#000000",
+      /*hvid*/
+      secondary: "#ffffff",
+      /*blue*/
+      blue: "#0000FF",
+      /*neon*/
+      neon: "#ABFF68",
+      /*grey*/
+      grey: "#EAEAEA",
+    },
+
+    fontFamily: {
+      montserrat: "'Montserrat', sans-serif;",
+      bebasneue: "'Bebas Neue', cursive",
+    },
+
+    extend: {
+      backgroundImage: {
+        gitter: "url('/img/gitter.png')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
